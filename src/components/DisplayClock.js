ab417631657f7e922916afe301f6326e2522cd17
CordiiT14 <cordelia.toennies@live.com>
2022-04-30 16:50:38 +0100

    counting down and displaying, countdown decremeanting twice every second

--- src/components/DisplayClock.js
+++ src/components/DisplayClock.js
@@ -2,13 +2,15 @@ import React, {useEffect} from 'react';
 
 const DisplayClock = ({workTime}) => {
 
-    useEffect(() => {
-        displayTimer()
-    }, [workTime])
+
 
     const displayTimer = () => {
-        let seconds = (workTime % 3600) % 60;
-        let minutes = parseInt(workTime % 3600) / 60;
+        // let seconds = (workTime % 3600) % 60;
+        // let minutes = parseInt(workTime / 3600) % 60;
+        // let hours = parseInt(workTime / 3600);
+
+        let seconds = workTime % 60;
+        let minutes = parseInt(workTime / 60 ) % 60;
         let hours = parseInt(workTime / 3600);
 
         return (`${addLeadingZeroes(hours)}:${addLeadingZeroes(minutes)}:${addLeadingZeroes(seconds)}`)
--- src/containers/Pomodoro.js
+++ src/containers/Pomodoro.js
@@ -8,41 +8,73 @@ const Pomodoro = () => {
     const [breakTime, setBreakTime] = useState(5);
     const [isClockRunning, setIsClockRunning] = useState(false);
 
+    // useEffect(() => {
+    //     runTimer()
+    // },[])
+
+    let timeLeftInSession = workTime;
+  
     let countdown;
-    
+
     const runTimer = () => {
+
             countdown = setInterval(() => {
-                stepDown();
-                console.log('hi')
+                stepDown()
+                console.log(timeLeftInSession);
             }, 1000)
+            
+
     }
 
     const stepDown = () => {
-        let temp = workTime - 1
-        setWorkTime(temp)
+
+        if(timeLeftInSession > 0 ) {
+            timeLeftInSession--
+            setWorkTime(timeLeftInSession)
+        } else {
+            clearInterval(countdown)
+        }
+        return timeLeftInSession;
     }
 
-    const startTimer = () => {
-        runTimer();
-        setIsClockRunning(true);
-    }
 
-    const pauseTimer = () => {
-        clearInterval(runTimer);
-        setIsClockRunning(false);
-    }
 
-    const resetTimer = () => {
-        clearInterval(countdown)
-        setWorkTime(1500);
-        setIsClockRunning(false);
-    }
+
+    // let countdown;
+    
+    // const runTimer = () => {
+    //         countdown = setInterval(() => {
+    //             stepDown();
+    //             console.log('hi')
+    //         }, 1000)
+    // }
+
+    // const stepDown = () => {
+    //     let temp = workTime - 1
+    //     setWorkTime(temp)
+    // }
+
+    // const startTimer = () => {
+    //     runTimer();
+    //     setIsClockRunning(true);
+    // }
+
+    // const pauseTimer = () => {
+    //     clearInterval(runTimer);
+    //     setIsClockRunning(false);
+    // }
+
+    // const resetTimer = () => {
+    //     clearInterval(countdown)
+    //     setWorkTime(1500);
+    //     setIsClockRunning(false);
+    // }
 
 
     return(
         <div>
             <DisplayClock workTime={workTime} />
-            <Controls isClockRunning={isClockRunning} startTimer={startTimer} pauseTimer={pauseTimer} resetTimer={resetTimer}/>
+            {/* <Controls isClockRunning={isClockRunning} startTimer={startTimer} pauseTimer={pauseTimer} resetTimer={resetTimer}/> */}
         </div>
     )
 };
